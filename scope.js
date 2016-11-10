3fb2061aee22fc33d33382251ba3c06940300e97
David Ebinger <davidebinger888@gmail.com>
2016-11-09 21:32:31 -1000

    Outerfunction + InnerFunction = 'Hello + World'

--- scope.js
+++ scope.js
@@ -2,11 +2,11 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,11 +17,10 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
-  innerFunction();
+  return innerFunction();
 }
-
 
 // This is a function that takes in a 2d-array (or matrix) and returns the sum of all elements
 // It's broken due to count variables colliding into each other.  Fix it!
